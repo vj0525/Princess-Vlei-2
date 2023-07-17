a212794c1455ac215186e1c12fa0ad00021358c7
vj0525 <vjanapati05@gmail.com>
2023-07-17 09:58:43 +0200

    Adding functionality for fauna survey

---
+++ Application Server/app/controllers/faunasurvey.controller.js
@@ -0,0 +1,114 @@
+const FaunaSurvey = require("../models/faunasurvey.model.js");
+
+// Create and Save a new Fauna Survey
+exports.create = (req, res) => {
+	// Validate request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  }
+
+
+
+  // Create a Organism
+  const faunasurvey = new FaunaSurvey({
+    faunaID: req.body.faunaID,
+    survey_date: req.body.survey_date,
+    sex: req.body.sex,
+    latitude: req.body.latitude,
+    longitude: req.body.longitude,
+    activity: req.body.activity,
+    life_stage: req.body.life_stage
+  });
+
+
+  // Save Fauna Survey in the database
+  FaunaSurvey.create(faunasurvey, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Fauna Survey."
+      });
+    else res.send(data);
+  });
+};
+
+// Retrieve all Fauna Surveys from the database (with condition).
+exports.findAll = (req, res) => {
+  const title = req.query.title;
+
+  FaunaSurvey.getAll(title, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving Fauna Surveys."
+      });
+    else res.send(data);
+  });
+};
+
+
+// Find a single Fauna Survey with a id
+exports.findOne = (req, res) => {
+    FaunaSurvey.findById(req.params.id, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found Fauna Surveys with id ${req.params.id}.`
+        });
+      } else {
+        res.status(500).send({
+          message: "Error retrieving Fauna Surveys with id " + req.params.id
+        });
+      }
+    } else res.send(data);
+  });
+};
+
+// Update a Fauna Survey identified by the id in the request
+exports.update = (req, res) => {
+    // Validate Request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  }
+
+  console.log(req.body);
+
+  FaunaSurvey.updateById(
+    req.params.id,
+    new FaunaSurvey(req.body),
+    (err, data) => {
+      if (err) {
+        if (err.kind === "not_found") {
+          res.status(404).send({
+            message: `Not found Fauna Survey with id ${req.params.id}.`
+          });
+        } else {
+          res.status(500).send({
+            message: "Error updating Fauna Survey with id " + req.params.id
+          });
+        }
+      } else res.send(data);
+    }
+  );
+};
+
+// Delete a Organism with the specified id in the request
+exports.delete = (req, res) => {
+    FaunaSurvey.remove(req.params.id, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found Fauna Survey with id ${req.params.id}.`
+        });
+      } else {
+        res.status(500).send({
+          message: "Could not delete Fauna Survey with id " + req.params.id
+        });
+      }
+    } else res.send({ message: `Fauna Survey was deleted successfully!` });
+  });
+};

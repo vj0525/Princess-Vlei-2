a699f8b368f761ea537db838241179f039d86b1d
Dimitri Vulopas <diminikov@gmail.com>
2023-07-20 09:45:33 +0200

    Adding getall functionality to flora survey

--- Application Server/app/routes/flora_survey.routes.js
+++ Application Server/app/routes/flora_survey.routes.js
@@ -9,6 +9,9 @@ module.exports = app => {
   // Retrieve all invasive Flora Survey
   router.get("/invasive", flora_survey.findAllInvasive);
 
+  // Retrieve all invasive Flora Survey
+  router.get("/", flora_survey.findAll);
+
   // Retrieve a single Flora Survey with id
   router.get("/:id", flora_survey.findOne);
 

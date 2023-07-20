abb11c7176273257145c464282adf04566c653fd
Sam Glickman <sglickman611@gmail.com>
2023-07-20 19:11:09 +0200

    Modification form for animals is now working for getting entries. Next steps will be to actually make editing work

--- Database Tool/src/pages/ModAnimal.js
+++ Database Tool/src/pages/ModAnimal.js
@@ -1,5 +1,6 @@
 import FancyButton from '../components/FancyButton';
 import TopBar from '../components/TopBar.js';
+import { useState } from 'react';
 import {Routes, Route, useNavigate} from 'react-router-dom';
 
 export default function ModAnimalPage(){
@@ -54,6 +55,8 @@ export default function ModAnimalPage(){
         return entry;
     }
     async function putUpDivs(dataFunc, key=""){
+        const button = document.getElementById('modSearchButton');
+        button.innerHTML = "Loading data...";
         const data = await dataFunc(key);
         const oldDataIfApplies = document.getElementById('data-container');
         if(oldDataIfApplies){
@@ -71,10 +74,9 @@ export default function ModAnimalPage(){
             console.log(newSet);
         }
         document.getElementById('forIDPurposes').appendChild(newSet);
+        button.innerHTML = "enter";
     }
     async function searchFilter(key){
-        let timeout = setTimeout(800);
-
         let keyString = key.toLowerCase();
         const response = await fetch(`https://pv-test.onrender.com/api/organism?name=${keyString}`, {
             method: 'GET',
@@ -88,14 +90,18 @@ export default function ModAnimalPage(){
         console.log(data);
         return data;
     }
-    
+    const [search, setSearch] = useState("");
     return (
         <div className="main-div">
             <TopBar />
-            <h2>Which animal would you like to modify:</h2>
-            <input className="formItems" type="text" onKeyUp={(key)=>putUpDivs(searchFilter,key.target.value)} placeholder="Search here for a specific animal"/>
-            <div className="body-div" id="forIDPurposes">
-                <button onClick={()=>putUpDivs(initializeAll)}>Click to start</button>
+            <h3>Type in an animal's name to pull up that data point.<br />
+            You can also type in part of a name to see multiple entries that match.<br />
+            If you'd like to see all entries, just hit enter with nothing in the search box</h3>
+            <div className="centerer">
+            <input className="formItems" id="modSearchBox" type="text" onChange={(key)=>setSearch(key.target.value)} placeholder="Search here for a specific animal"/>
+            <button id="modSearchButton" onClick={()=>putUpDivs(searchFilter,search)}>enter</button>
+            </div>
+            <div id="forIDPurposes">
                 <div id="data-container">
                 </div>
             </div>

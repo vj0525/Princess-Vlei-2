44f096191703475188f59e221d667b96611caff9
Dimitri Vulopas <diminikov@gmail.com>
2023-07-25 14:58:21 +0200

    Changing database connections to pooled

--- Application-Server/app/models/db.js
+++ Application-Server/app/models/db.js
@@ -1,18 +1,11 @@
 const mysql = require("mysql");
 const dbConfig = require("../config/db.config.js");
 
-// Create a connection to the database
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   database: dbConfig.DB
 });
 
-// open the MySQL connection
-connection.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
-});
-
-module.exports = connection;
+module.exports = pool;
